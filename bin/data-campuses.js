b49cab971050f72beadd7dd609d8a17661799a2c
Maxence Bouret <maxence.bouret@gmail.com>
2019-02-01 11:29:21 +0100

    New iteration

---
+++ bin/data-campuses.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    city: 'Berlin',
+    gm: 'Michael L'
+  },
+  {
+    city: 'Paris',
+    gm: 'Karim F'
+  },
+]
--- models/Student.js
+++ models/Student.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const studentSchema = new Schema({
-  // _id: Schema.Types.ObjectId,
   firstName: String,
   lastName: String,
   _campus: Schema.Types.ObjectId, // starts with `_` because it's an ObjectId (convention)
